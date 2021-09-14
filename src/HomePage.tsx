33065524143795761d80d9c1e64bae502b333557
Zahra Behfarshad <zbehfarshad@gmail.com>
2021-09-13 22:53:44 -0700

    Passing a render prop to QuestionList to output the title of question

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -10,7 +10,10 @@ export const HomePage = () => {
       <div>
         <PageTitle>Unanswered Questions</PageTitle>
         <button>Ask a question</button>
-        <QuestionList data={getUnansweredQuestions()} />
+        <QuestionList
+          data={getUnansweredQuestions()}
+          renderItem={(question) => <div>{question.title}</div>}
+        />
       </div>
     </Page>
   );
