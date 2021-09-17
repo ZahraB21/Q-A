67494fc8afc19442cf340e03170c6140baa2d5da
Zahra Behfarshad <zbehfarshad@gmail.com>
2021-09-17 02:25:10 -0700

    Removing .css files and creating related .module.css files and importing them in related components

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { UserIcon } from './Icons';
+import styles from './Header.module.css';
 
 const Header = () => {
   const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
   };
   return (
-    <div>
+    <div className={styles.container}>
       <a href="./">Q & A</a>
       <input
         placeholder="Search ..."
