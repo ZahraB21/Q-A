14c49e1148b9e07648396230b453b5c84b756080
Zahra Behfarshad <zbehfarshad@gmail.com>
2021-09-16 21:55:00 -0700

    Using useEffect and useState to get the unanswered questions and render them appropriately

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { QuestionList } from './QuestionList';
-import { getUnansweredQuestions } from './QuestionsData';
+import { getUnansweredQuestions, QuestionData } from './QuestionsData';
 import { Page } from './Page';
 import { PageTitle } from './PageTitle';
 
 export const HomePage = () => {
+  const [questions, setQuestions] = useState<QuestionData[]>([]);
+  const [questionLoading, setQuestionLoading] = useState(true);
+  useEffect(() => {
+    const doGetUnansweredQuestions = async () => {
+      const unansweredQuestions = await getUnansweredQuestions();
+      setQuestions(unansweredQuestions);
+      setQuestionLoading(false);
+    };
+    doGetUnansweredQuestions();
+  }, []);
+  console.log('render');
   return (
     <Page>
       <div>
         <PageTitle>Unanswered Questions</PageTitle>
         <button>Ask a question</button>
-        <QuestionList
-          data={getUnansweredQuestions()}
-          // renderItem={(question) => <div>{question.title}</div>}
-        />
+        {questionLoading ? (
+          <div>Loading</div>
+        ) : (
+          <QuestionList
+            data={questions}
+            // renderItem={(question) => <div>{question.title}</div>}
+          />
+        )}
       </div>
     </Page>
   );
