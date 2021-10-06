cbd0a012c6c02c0793bed44cbb210b680180e6d4
Zahra Behfarshad <zbehfarshad@gmail.com>
2021-10-05 21:09:09 -0700

    Importing css and adding necessary styling to elements

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,3 +1,6 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
 import React from 'react';
 import { PageTitle } from './PageTitle';
 
@@ -8,7 +11,13 @@ interface Props {
 
 export const Page = ({ title, children }: Props) => {
   return (
-    <div>
+    <div
+      css={css`
+        margin: 50px auto 20px auto;
+        padding: 30px 20px;
+        max-width: 600px;
+      `}
+    >
       {title && <PageTitle>{title}</PageTitle>}
       {children}
     </div>
