2e89b8de7d606db64472bc574bdc5ee6e76c8c48
Zahra Behfarshad <zbehfarshad@gmail.com>
2021-10-13 16:17:01 -0700

    Creating a new page component as the name implies

---
+++ src/AskPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Page } from './Page';
+
+export const AskPage = () => {
+  return (
+    <Page title="Ask a Question">
+      <p>This is the ask a question page</p>
+    </Page>
+  );
+};
